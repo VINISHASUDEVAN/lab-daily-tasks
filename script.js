6321c8888f8d549d089f200d9fc1b8ea1db4164b
VINISHASUDEVAN <sudevanvinisha@gmail.com>
2025-07-24 21:39:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var name = "vinisha";
+let age = 25;
+const country = "India";
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Country:", country);
+name = "Sudevan";
+age=26;
+console.log("Updated Name:", name);
+console.log("Updated Age:", age);
